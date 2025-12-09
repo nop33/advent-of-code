e54f607bca0b270dcd7619f07b593824f46097fc
Ilias Trichopoulos <hlias.nop@gmail.com>
2025-12-09 19:43:02 +0800

    Solve day 5 part 2

--- 2025/day5/main.ts
+++ 2025/day5/main.ts
@@ -5,22 +5,43 @@ const part = getPartArgument()
 const main = async () => {
   const rl = createReadlineInterfaceFromFile('input.txt')
 
-  const freshIngredientsRanges: Array<Array<number>> = []
+  const freshIngredientsRanges: Array<[number, number]> = []
   let totalFreshAvailableIngredients = 0
 
   for await (const line of rl) {
     if (line.includes('-')) {
-      freshIngredientsRanges.push(line.split('-').map(Number))
-    } else if (part === 1 &&line.length > 0) {
+      const [start, end] = line.split('-').map(Number)
+      freshIngredientsRanges.push([start, end])
+    } else if (part === 1 && line.length > 0) {
       const id = parseInt(line)
       const isWithinARange = freshIngredientsRanges.some((range) => id >= range[0] && id <= range[1])
       totalFreshAvailableIngredients += isWithinARange ? 1 : 0
     }
   }
 
-  if (part === 1) {
-    console.log('Fresh ingredients:', totalFreshAvailableIngredients)
+  if (part === 2) {
+    freshIngredientsRanges.sort((a, b) => a[0] !== b[0] ? a[0] - b[0] : a[1] - b[1])
+
+    totalFreshAvailableIngredients = 0
+    const latestRange = { start: 0, end: 0 }
+
+    for (let i = 0; i < freshIngredientsRanges.length; i++) {
+      const [start, end] = freshIngredientsRanges[i]
+
+      if (start > latestRange.end) {
+        totalFreshAvailableIngredients += end - start + 1
+        latestRange.start = start
+        latestRange.end = end
+      } else if (start >= latestRange.start && end <= latestRange.end) {
+        continue
+      } else if (start >= latestRange.start && start <= latestRange.end) {
+        totalFreshAvailableIngredients += end - latestRange.end
+        latestRange.end = end
+      }
+    }
   }
+
+  console.log('Fresh ingredients:', totalFreshAvailableIngredients)
 }
 
 main()
